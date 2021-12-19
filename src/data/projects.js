6cedb6cdcb679b47332fc055652dcf5c6e50ec4d
Baitemir Asanbaev <baitemir1706@gmail.com>
2021-12-19 13:55:44 +0600

    added more projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,11 @@
 import music from "../images/slider-images/music-maker.png"
 import kanye from "../images/slider-images/kanye-western.png"
 import dragon from "../images/slider-images/dragon.png"
+import altyn from "../images/slider-images/altyn.png"
+import apple from "../images/slider-images/apple.png"
+import fast from "../images/slider-images/fast.png"
+import bottle from "../images/slider-images/bottle.png"
+import kin from "../images/slider-images/mizusashi.png"
 
 export const projects = [
   {
@@ -17,5 +22,30 @@ export const projects = [
     title: "Animated Dragon",
     img: dragon,
     link: "https://dragon-animation.netlify.app/"
-  }
+  },
+  {
+    title: "Altyn Kumara",
+    img: altyn,
+    link: "https://altyn-kumara.netlify.app"
+  },
+  {
+    title: "Apple Layout",
+    img: apple,
+    link: "https://apple-layout.netlify.app"
+  },
+  {
+    title: "Fast typing",
+    img: fast,
+    link: "https://fast-type-test.netlify.app"
+  },
+  {
+    title: "Bottle Game",
+    img: bottle,
+    link: "https://game-bottle.netlify.app"
+  },
+  {
+    title: "Kin no mizusashi",
+    img: kin,
+    link: "https://kin-no-mizusashi.netlify.app"
+  },
 ]
